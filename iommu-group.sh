c54fdd43f09056aa553aafcf844c966de4ab9f39
Stefan <stefanleh@users.noreply.github.com>
2022-04-20 12:16:12 +0200

    Update iommu-group.sh
    More readable (IMHO) version of the IOMMU groups script.

--- iommu-group.sh
+++ iommu-group.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 shopt -s nullglob
-for d in /sys/kernel/iommu_groups/*/devices/*; do
-    n=${d#*/iommu_groups/*}; n=${n%%/*}
-    printf 'IOMMU Group %s ' "$n"
-    lspci -nns "${d##*/}"
+for g in /sys/kernel/iommu_groups/*; do
+    echo "IOMMU Group ${g##*/}:"
+    for d in $g/devices/*; do
+        echo -e "\t$(lspci -nns ${d##*/})"
+    done;
 done;
