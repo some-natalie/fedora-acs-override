3e514b4b1dc61fd4e00358c92120768fd78a08ca
Stefan <stefanleh@users.noreply.github.com>
2022-04-24 22:51:08 +0200

    Update iommu-group.sh
    Co-authored-by: Natalie Somersall <some-natalie@github.com>

--- iommu-group.sh
+++ iommu-group.sh
@@ -2,7 +2,7 @@
 shopt -s nullglob
 for g in /sys/kernel/iommu_groups/*; do
     echo "IOMMU Group ${g##*/}:"
-    for d in $g/devices/*; do
-        echo -e "\t$(lspci -nns ${d##*/})"
+    for d in "$g"/devices/*; do
+        echo -e "\t$(lspci -nns "${d##*/}")"
     done;
 done;
